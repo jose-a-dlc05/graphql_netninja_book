cb7a09beaa81121abb3787f326c181907402e416
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-01-21 01:48:40 -0500

    Installed graphql and express-graphql to bind express with it

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+// express-graphql is installed because express by itself does not know how to handle graphql
+
 const app = express(); // invoke express
+
 const PORT = 4000; // Define and initialize the PORT variable to hold value of 4000
+
+// bind express with graphql - https://graphql.org/graphql-js/express-graphql/
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		// pass in schema property
+	})
+);
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);

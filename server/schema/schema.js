c0ad7c47c03cef780ad7557735d22d434a799481
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-01-23 12:24:35 -0500

    Added the GraphQLList to AuthorType for relating authors to books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,6 +7,7 @@ const {
 	GraphQLSchema,
 	GraphQLID,
 	GraphQLInt,
+	GraphQLList,
 } = graphql;
 
 // GraphQLObjectType is used for defining objects
@@ -14,15 +15,19 @@ const {
 // GraphQLSchema is a type for Query and Mutation
 
 const books = [
-	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' }, // Added authId here for access to authors
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
 	{ name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+	{ name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
 	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+	{ name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+	{ name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+	// Added authId here for access to authors
 ];
 
 const authors = [
-	{ name: 'Junot Diaz', age: 54, id: '1' },
-	{ name: 'Richard Brown', age: 36, id: '2' },
-	{ name: 'Marcus Green', age: 46, id: '3' },
+	{ name: 'Patrick Rothfuss', age: 44, id: '1' },
+	{ name: 'Brandon Sanderson', age: 42, id: '2' },
+	{ name: 'Terry Pratchett', age: 66, id: '3' },
 ];
 
 const BookType = new GraphQLObjectType({
@@ -47,6 +52,12 @@ const AuthorType = new GraphQLObjectType({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve(parent, args) {
+				return _.filter(books, { authorId: parent.id });
+			},
+		},
 	}),
 });
 
